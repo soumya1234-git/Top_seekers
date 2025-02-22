a26d45449109e64141783f4bbb24db3576a3095e
soumya1234-git <soumyachandel5@gmail.com>
2025-02-22 08:41:06 +0000

    Pushed

--- script.js
+++ script.js
@@ -2,3 +2,15 @@ let header = document.querySelector('header');
 window.addEventListener('scroll', () => {
     header.classList.toggle('active',window.scrollY > 0);
 });
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("year").textContent = new Date().getFullYear();
+
+    document.querySelector(".newsletter button").addEventListener("click", function() {
+        let email = document.querySelector(".newsletter input").value;
+        if(email) {
+            alert("Thank you for subscribing!");
+        } else {
+            alert("Please enter a valid email.");
+        }
+    });
+});
